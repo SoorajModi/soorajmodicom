c7dca510c15e4b73c4b8b87efa1bfb8310ca8e62
SoorajModi <soomodi@gmail.com>
2022-01-12 02:14:34 -0500

    Add more links + change color

--- src/pages/header/components/links.tsx
+++ src/pages/header/components/links.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Links() {
   return (
-    <div>
-      <a href="https://github.com/SoorajModi">Github</a>
+    <div className="text-neutral-400">
+      <a href="https://github.com/SoorajModi">Github </a>
+      <a href="https://www.linkedin.com/in/soorajmodi/">LinkedIn </a>
+      <a href="/">Resume </a>
+      <a href="/">Discord </a>
+      <a href="/">Spotify </a>
     </div>
   );
 }
