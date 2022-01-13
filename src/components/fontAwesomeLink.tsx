008b8b535a538a48be2c2a0e307ace9c3e53e4c6
SoorajModi <soomodi@gmail.com>
2022-01-13 18:26:45 -0500

    Add hover behaviour for links

--- src/components/fontAwesomeLink.tsx
+++ src/components/fontAwesomeLink.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ReactTooltip from 'react-tooltip';
 
 interface Props {
     icon: any,
     href: string,
+    circleStyle: any,
+    iconStyle: any,
+    tooltip: string,
 }
 
-function FontAwesomeLink({ icon, href }: Props) {
+function FontAwesomeLink({
+  icon, href, circleStyle, iconStyle, tooltip,
+}: Props) {
   return (
-    <div className="flex rounded-full bg-gray-500 h-12 w-12">
-      <a href={href} target="_blank" rel="noreferrer" className="grid place-items-center w-full text-black">
+    <div className={`flex rounded-full bg-gray-500 h-12 w-12 ${circleStyle}`}>
+      <a href={href} target="_blank" rel="noreferrer" data-tip={tooltip} className={`grid place-items-center w-full text-black ${iconStyle}`}>
         <FontAwesomeIcon icon={icon} size="2x" />
+        <ReactTooltip place="bottom" effect="solid" />
       </a>
     </div>
   );
--- src/pages/header/components/links.tsx
+++ src/pages/header/components/links.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   faDiscord,
   faGithub,
-  faLinkedin, faSpotify,
+  faLinkedin,
+  faSpotify,
 } from '@fortawesome/free-brands-svg-icons';
 import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeLink } from '../../../components';
@@ -10,11 +11,41 @@ import { FontAwesomeLink } from '../../../components';
 function Links() {
   return (
     <div className="text-neutral-400 space-x-5 grid grid-rows-1 grid-cols-5 justify-items-center">
-      <FontAwesomeLink icon={faGithub} href="https://github.com/SoorajModi" />
-      <FontAwesomeLink icon={faLinkedin} href="https://www.linkedin.com/in/soorajmodi/" />
-      <FontAwesomeLink icon={faFileAlt} href="/" />
-      <FontAwesomeLink icon={faDiscord} href="https://discordapp.com/users/356956593293754368" />
-      <FontAwesomeLink icon={faSpotify} href="https://open.spotify.com/user/soorajm" />
+      <FontAwesomeLink
+        icon={faGithub}
+        href="https://github.com/SoorajModi"
+        tooltip="GitHub"
+        circleStyle="hover:bg-white"
+        iconStyle="hover:text-black"
+      />
+      <FontAwesomeLink
+        icon={faLinkedin}
+        href="https://www.linkedin.com/in/soorajmodi/"
+        tooltip="LinkedIn"
+        circleStyle="hover:bg-white"
+        iconStyle="hover:text-blue-500"
+      />
+      <FontAwesomeLink
+        icon={faFileAlt}
+        href="/"
+        tooltip="Resume"
+        circleStyle="hover:bg-red-500"
+        iconStyle="hover:text-white"
+      />
+      <FontAwesomeLink
+        icon={faDiscord}
+        href="https://discordapp.com/users/356956593293754368"
+        tooltip="Discord"
+        circleStyle="hover:bg-purple-500"
+        iconStyle="hover:text-white"
+      />
+      <FontAwesomeLink
+        icon={faSpotify}
+        href="https://open.spotify.com/user/soorajm"
+        tooltip="Spotify"
+        circleStyle="hover:bg-black"
+        iconStyle="hover:text-green-500"
+      />
     </div>
   );
 }
