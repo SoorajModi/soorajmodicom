590055cc4c5cab17255e12ecff1d823064c0d141
SoorajModi <soomodi@gmail.com>
2022-01-14 23:04:13 -0500

    Support responsive design

--- src/pages/home/components/message.tsx
+++ src/pages/home/components/message.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Message() {
   return (
-    <h3 className="text-xl grid justify-items-center pb-10">
-      <div className="w-1/3">
+    <h3 className="text-base md:text-xl grid justify-items-center pb-10">
+      <div className="w-9/12 md:w-96 lg:w-1/3">
         Hi, my name is Sooraj, and welcome to my website! I am a full stack software developer
         who loves taking on new challenges and working on fun projects that improve our world!
-        Take a look around and feel free to reach out.
+        Feel free to reach out.
       </div>
     </h3>
   );
--- src/pages/home/components/name.tsx
+++ src/pages/home/components/name.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Name() {
-  return <h1 className="text-6xl pb-2">Sooraj Modi</h1>;
+  return <h1 className="text-4xl sm:text-6xl pb-1 sm:pb-2">Sooraj Modi</h1>;
 }
 
 export default Name;
