7841827ab61b23cb738cce3de85ee665050f7fbe
SoorajModi <soomodi@gmail.com>
2022-01-16 18:43:20 -0500

    Add firebase config files

--- src/pages/home/components/message.tsx
+++ src/pages/home/components/message.tsx
@@ -4,7 +4,7 @@ function Message() {
   return (
     <h3 className="text-base md:text-xl grid justify-items-center pb-10">
       <div className="w-9/12 md:w-96 lg:w-1/3">
-        Hi, my name is Sooraj, and welcome to my website! I am a full stack software developer
+        Hi my name is Sooraj, and welcome to my website. I am a full stack software developer
         who loves taking on new challenges and working on fun projects that improve our world!
         Feel free to reach out.
       </div>
