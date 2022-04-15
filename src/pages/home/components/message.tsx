81e9847f813774e3df32687e388e7fe22bbd28ef
SoorajModi <soomodi@gmail.com>
2022-04-15 00:28:40 -0400

    Refactor message component

--- src/pages/home/components/message.tsx
+++ src/pages/home/components/message.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 function Message() {
   return (
-    <h3 className="text-base md:text-xl grid justify-items-center pb-10">
-      <div className="w-9/12 md:w-96 lg:w-1/3">
-        Hi my name is Sooraj, and welcome to my website. I am a full stack software developer
-        who loves taking on new challenges and working on fun projects that improve our world!
-        Feel free to reach out.
-      </div>
-    </h3>
+    <div className="text-base md:text-xl flex justify-center align-center pb-10 w-96">
+      Hi my name is Sooraj, and welcome to my website. I am a full stack software developer
+      who loves taking on new challenges and working on fun projects that improve our world!
+      Feel free to reach out.
+    </div>
   );
 }
 
