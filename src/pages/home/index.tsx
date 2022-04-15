8bc990026754122c2e872cd8ef16752927e844c0
Sooraj <soomodi@gmail.com>
2022-04-15 01:07:39 -0400

    Refactor home page (#27)
    * Update dependencies
    
    * Remove spotify
    
    * Fix manifest file
    
    * Refactor links component
    
    * Refactor message component
    
    * Refactor home page
    
    * CSS fixes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {
-  Links, Message, Name, Title, Image,
-} from './components';
+import Profile from './assets/profile.png';
+import Links from './components/links';
 
 function Home() {
   return (
-    <div className="bg-sooraj-blue h-screen min-h-[50rem] grid place-items-center">
-      <div className="text-center text-white m-1">
-        <Image />
-        <Name />
-        <Title />
-        <Message />
-        <Links />
+    <div className="bg-sooraj-blue h-screen min-h-[50rem] flex justify-center items-center flex-col text-center text-white">
+      <img src={Profile} alt="Sooraj Modi" width="300" className="inline-block py-2" />
+      <h1 className="text-4xl sm:text-6xl py-2">Sooraj Modi</h1>
+      <h2 className="text-neutral-400 text-lg sm:text-2xl pb-5">Software Developer</h2>
+      <div className="text-base sm:text-xl flex justify-center align-center pb-10 w-96">
+        Hi my name is Sooraj, and welcome to my website. I am a full stack software developer
+        who loves taking on new challenges and working on fun projects that improve our world!
+        Feel free to reach out.
       </div>
+      <Links />
     </div>
   );
 }
--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Links from '../pages/home/components/links';
+import Home from '../pages/home';
 
 describe('Links', () => {
   it('should render Links component', () => {
-    const component = shallow(<Links />);
+    const component = shallow(<Home />);
     expect(component.length).toEqual(1);
   });
 
   it('should match Links snapshot', () => {
-    const component = shallow(<Links />);
+    const component = shallow(<Home />);
     expect(component).toMatchSnapshot();
   });
 });
