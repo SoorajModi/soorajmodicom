b4559b8fb95ff47e37c22ead39ec5e9c0e6ce2fb
SoorajModi <soomodi@gmail.com>
2022-04-15 00:19:57 -0400

    Refactor links component

--- src/pages/home/components/links.tsx
+++ src/pages/home/components/links.tsx
@@ -9,53 +9,60 @@ import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import FontAwesomeLink from './fontAwesomeLink';
 
 function Links() {
+  const list = [
+    {
+      icon: faGithub,
+      href: 'https://github.com/SoorajModi',
+      tooltip: 'GitHub',
+      circleStyle: 'hover:bg-white',
+      iconStyle: 'hover:text-black',
+      download: false,
+    },
+    {
+      icon: faLinkedin,
+      href: 'https://www.linkedin.com/in/soorajmodi/',
+      tooltip: 'LinkedIn',
+      circleStyle: 'hover:bg-white',
+      iconStyle: 'hover:text-linkedin-blue',
+      download: false,
+    },
+    {
+      icon: faTwitter,
+      href: 'https://twitter.com/SoorajModi',
+      tooltip: 'Twitter',
+      circleStyle: 'hover:bg-white',
+      iconStyle: 'hover:text-twitter-blue',
+      download: false,
+    },
+    {
+      icon: faDiscord,
+      href: 'https://discordapp.com/users/356956593293754368',
+      tooltip: 'Discord',
+      circleStyle: 'hover:bg-discord-purple',
+      iconStyle: 'hover:text-white',
+      download: false,
+    },
+    {
+      icon: faFileAlt,
+      href: '/SoorajModiResume.pdf',
+      tooltip: 'Resume',
+      circleStyle: 'hover:bg-red-500',
+      iconStyle: 'hover:text-white',
+      download: true,
+    },
+  ];
+
   return (
-    <div className="text-neutral-400 grid justify-items-center">
-      <div className="w-80 sm:w-10/12 lg:w-1/3
-        grid grid-rows-3 grid-cols-2 sm:grid-rows-2 sm:grid-cols-3 md:grid-rows-1 md:grid-cols-6
-        justify-items-center"
-      >
-        <FontAwesomeLink
-          icon={faGithub}
-          href="https://github.com/SoorajModi"
-          tooltip="GitHub"
-          circleStyle="hover:bg-white"
-          iconStyle="hover:text-black"
-          download={false}
-        />
-        <FontAwesomeLink
-          icon={faLinkedin}
-          href="https://www.linkedin.com/in/soorajmodi/"
-          tooltip="LinkedIn"
-          circleStyle="hover:bg-white"
-          iconStyle="hover:text-linkedin-blue"
-          download={false}
-        />
-        <FontAwesomeLink
-          icon={faTwitter}
-          href="https://twitter.com/SoorajModi"
-          tooltip="Twitter"
-          circleStyle="hover:bg-white"
-          iconStyle="hover:text-twitter-blue"
-          download={false}
-        />
-        <FontAwesomeLink
-          icon={faDiscord}
-          href="https://discordapp.com/users/356956593293754368"
-          tooltip="Discord"
-          circleStyle="hover:bg-discord-purple"
-          iconStyle="hover:text-white"
-          download={false}
-        />
-        <FontAwesomeLink
-          icon={faFileAlt}
-          href="/SoorajModiResume.pdf"
-          tooltip="Resume"
-          circleStyle="hover:bg-red-500"
-          iconStyle="hover:text-white"
-          download
-        />
-      </div>
+    <div className="text-neutral-400 w-full flex justify-center align-center">
+      {
+        list.map((link, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div className="flex px-4" key={index}>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <FontAwesomeLink {...link} />
+          </div>
+        ))
+      }
     </div>
   );
 }
