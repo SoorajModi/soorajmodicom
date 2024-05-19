56f2d4b18d8db30132f0f95ceb29c53d3f6374f3
SoorajModi <soomodi@gmail.com>
2024-05-19 00:26:10 -0400

    Add basic page structure

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -4,12 +4,33 @@ import { render, screen, within } from '@testing-library/react'
 import Home from './page'
  
 describe('Page', () => {
-  it('renders getStarted message', () => {
+  it('renders name', () => {
     render(<Home />)
  
-    const getStarted = screen.getByText('Get started by editing', { exact: false });
+    const name = screen.getByText('Sooraj Modi', { exact: true });
  
-    expect(getStarted).toBeInTheDocument()
-    expect(getStarted).toHaveTextContent('Get started by editing');
+    expect(name).toBeInTheDocument()
+    expect(name).toHaveTextContent('Sooraj Modi');
+  })
+
+  it('renders title', () => {
+    render(<Home />)
+ 
+    const title = screen.getByText('Software Developer', { exact: true });
+ 
+    expect(title).toBeInTheDocument()
+    expect(title).toHaveTextContent('Software Developer');
+  })
+
+  it('renders profile image', () => {
+    render(<Home />)
+ 
+    const image = screen.getByRole('img');
+ 
+    expect(image).toBeInTheDocument()
+    expect(image).toHaveAttribute('src', expect.stringMatching('profile\.png'))
+    expect(image).toHaveAttribute('alt', 'Sooraj Modi profile picture')
+    expect(image).toHaveAttribute('height', "300")
+    expect(image).toHaveAttribute('width', "300")
   })
 })
